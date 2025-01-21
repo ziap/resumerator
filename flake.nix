6ab2ad4d1f212348f3f4e0dfce3ea12645f4f7fe
Zap <67962871+ziap@users.noreply.github.com>
2025-01-22 00:12:37 +0700

    Add proper compiler/linker env vars

--- flake.nix
+++ flake.nix
@@ -10,38 +10,43 @@
     pkgs = import nixpkgs { inherit system; };
   in {
     devShell.${system} = let
-      # Importing cross compilation packages
-      pkgsCross = {
-        mingw = import pkgs.path {
-          system = system;
-          crossSystem = {
-            config = "x86_64-w64-mingw32";
-          };
-        };
-
-        musl = import pkgs.path {
-          system = system;
-          crossSystem = {
-            config = "x86_64-unknown-linux-musl";
-          };
-        };
+      targetName = {
+        mingw = "x86_64-w64-mingw32";
+        musl = "x86_64-unknown-linux-musl";
       };
+
+      # Generate the cross compilation packages import
+      pkgsCross = builtins.mapAttrs (name: value: import pkgs.path {
+        system = system;
+        crossSystem = {
+          config = value;
+        };
+      }) targetName;
+
+      # Grab the corresponding C compiler binaries
+      ccPkgs = builtins.mapAttrs (name: value: value.stdenv.cc) pkgsCross;
+      cc = builtins.mapAttrs (name: value: "${value}/bin/${targetName.${name}}-cc") ccPkgs;
 
       wine = pkgs.wineWowPackages.stable;
     in pkgs.mkShell {
       buildInputs = [
         pkgs.rustup
-        pkgsCross.mingw.stdenv.cc
-        pkgsCross.musl.stdenv.cc
         wine
         pkgs.chromium
-      ];
+      ] ++ builtins.attrValues ccPkgs;
 
       # Set the default target to the first available target
       CARGO_BUILD_TARGET = let
         toolchain = builtins.readFile ./rust-toolchain.toml;
         targets = (builtins.fromTOML toolchain).toolchain.targets;
       in builtins.head targets;
+
+      # Set compilers and linkers for C libraries
+      CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = cc.musl;
+      CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = cc.mingw;
+
+      CC_x86_64_unknown_linux_musl = cc.musl;
+      CC_x86_64_pc_windows_gnu = cc.mingw;
 
       # Use wine for `cargo run`, `cargo test`, etc.
       CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = "${wine}/bin/wine64";
